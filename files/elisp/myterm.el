ce0a8fe76feede7a60c54bac64028f297bd129e2
Thierry Delamare <t.delamare@epiconcept.fr>
2016-04-26 17:42:11 +0200

    myterm.el

---
+++ files/elisp/myterm.el
@@ -0,0 +1,28 @@
+(defun buffer-exists (name) (not (eq nil (get-buffer name))))
+
+(defun mk-term-buffer-name ()
+  (cond
+   ((or dired-directory buffer-file-name)
+    (concat
+     "term "
+     (file-name-nondirectory
+      (directory-file-name
+       (or dired-directory (file-name-directory buffer-file-name))))))
+   ("ansi-term")))
+
+(defun myterm (&optional buffer)
+  "Call `ansi-term', auto naming buffer from either current
+directory of (dired or file name) or provided name. If prefixed
+use default `*ansi-term*', if prefixes twice ask for a buffer
+name"
+  (interactive
+   (cond
+    ((equal current-prefix-arg '(16))
+     (list (read-buffer "Term Buffer: " (list "ansi-term"))))
+    ((equal current-prefix-arg '(4)) (list "ansi-term"))))
+  (let* ((buffer-name (or buffer (mk-term-buffer-name)))
+	 (term-buffer-name (concat "*" buffer-name "*")))
+    (cond 
+     ((buffer-exists term-buffer-name)
+      (switch-to-buffer term-buffer-name))
+     (t (ansi-term "/bin/bash" buffer-name)))))
